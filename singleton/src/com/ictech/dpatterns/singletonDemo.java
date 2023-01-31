8052e5cba4ce73c2fb28d5eeffb441d5ea2ece6b
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-01-31 08:43:08 +0530

    lazy called and done

--- singleton/src/com/ictech/dpatterns/singletonDemo.java
+++ singleton/src/com/ictech/dpatterns/singletonDemo.java
@@ -5,5 +5,10 @@ public class singletonDemo {
     public static void main(String[] args) {
             EagerSingleton eager=EagerSingleton.getInstance();
                 eager.showMessage();
+
+
+
+                LazySingleton lazy=LazySingleton.getInstance();
+                lazy.showMessage();
     }
 }
