3d2c45e9ac32aa1dac5df35596d56baa8e862bf5
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-02 21:25:55 +0530

    bILLPUGH singleton done

--- singleton/src/com/ictech/dpatterns/BillPughSingleton.java
+++ singleton/src/com/ictech/dpatterns/BillPughSingleton.java
@@ -1,6 +1,24 @@
 package com.ictech.dpatterns;
 
 public class BillPughSingleton {
-    //make condtructor private
+    //make the constructor private
+    private BillPughSingleton() {
+    }
 
+    //create the inner class
+    private static class Helper{
+        //create the private static final object
+        private static final BillPughSingleton bps = new BillPughSingleton();
+    }
+
+    //return the only object available
+    public static BillPughSingleton getInstance() {
+        return Helper.bps;
+    }
+
+    //Sample method to test
+    public void showMessage(){
+        System .out.println("Hello From Bill Pugh Singleton...!!!");
+    }
 }
+
--- singleton/src/com/ictech/dpatterns/singletonDemo.java
+++ singleton/src/com/ictech/dpatterns/singletonDemo.java
@@ -4,11 +4,11 @@ public class singletonDemo {
 
     public static void main(String[] args) {
             EagerSingleton eager=EagerSingleton.getInstance();
-                eager.showMessage();
+            eager.showMessage();
+
+            LazySingleton lazy=LazySingleton.getInstance();
+            lazy.showMessage();
 
 
-
-                LazySingleton lazy=LazySingleton.getInstance();
-                lazy.showMessage();
     }
 }
