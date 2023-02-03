2dba55e1a61a98b91119cdef0655a0ce194a703e
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-03 06:26:33 +0530

    finish StaticSingleton

--- singleton/src/com/ictech/dpatterns/StaticSingleton.java
+++ singleton/src/com/ictech/dpatterns/StaticSingleton.java
@@ -1,4 +1,30 @@
 package com.ictech.dpatterns;
 
 public class StaticSingleton {
+    //create private instance
+    private static StaticSingleton ss;
+
+
+    //make constructor private
+
+
+    private StaticSingleton() {
+    }
+
+    //making static block with exception handling
+    static {
+        try{
+            ss=new StaticSingleton();
+        }
+        catch(Exception e){
+            throw new RuntimeException("show error");
+        }
+    }
+
+    //return only object available
+     public static StaticSingleton getInstance(){
+        return ss;
+     }
 }
+
+
