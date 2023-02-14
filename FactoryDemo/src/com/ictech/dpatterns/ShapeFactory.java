4f9bb215d49a44cc00a4c6a56da822cf34d86227
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-14 23:56:13 +0530

    added factory
    shapeFactory

--- FactoryDemo/src/com/ictech/dpatterns/ShapeFactory.java
+++ FactoryDemo/src/com/ictech/dpatterns/ShapeFactory.java
@@ -5,6 +5,16 @@ public class ShapeFactory {
     public Shape getshape (String name){
         if (name==null){
             return null;
-        }else if()
+        }else if(name.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+        }else if(name.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        }else if(name.equalsIgnoreCase("SQUARE")){
+            return new Square();
+        }
+        return null;
+
     }
+
+
 }
