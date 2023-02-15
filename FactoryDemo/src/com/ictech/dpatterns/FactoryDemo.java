bdaf1ddf01860436b1c5e887bc840d2d5ccef23c
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-15 09:30:08 +0530

    factory dpattern finished

--- FactoryDemo/src/com/ictech/dpatterns/FactoryDemo.java
+++ FactoryDemo/src/com/ictech/dpatterns/FactoryDemo.java
@@ -3,7 +3,10 @@ package com.ictech.dpatterns;
 public class FactoryDemo {
 
     public static void main(String[] args) {
+        ShapeFactory sf=new ShapeFactory();
 
+        Shape Square=sf.getshape("Square");
+        Square.draw();
     }
 
 }
